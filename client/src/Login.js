9b65f96031c743c6229457ebee143440d6a2ebfa
BARAA AHMAD <bahmad@ou.edu>
2022-04-15 08:34:50 -0500

    more code clean up

--- client/src/Login.js
+++ client/src/Login.js
@@ -8,7 +8,7 @@ const AUTH_URL =
   "https://accounts.spotify.com/authorize?client_id=52c66e4315d64fad9d30fc06a14e7ff1&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read";
 
 export default function Login({ code }) {
-  // const accessToken = useAuth(code);
+
 
   function authorize() {
     var body = {
